08ba48fb9315e3041cffbc9c7d6fcf04d745b2bc
Ingo Steinke <ingosteinke@gmx.de>
2021-09-22 22:44:31 +0200

    implement migration update (create table)

--- src/Migration/Migration1632157456.php
+++ src/Migration/Migration1632157456.php
@@ -14,11 +14,28 @@ class Migration1632157456 extends MigrationStep
 
     public function update(Connection $connection): void
     {
-        // implement update
+        $connection->executeStatement("CREATE TABLE IF NOT EXISTS `ingos_ingorance` (
+                `id` BINARY(16) NOT NULL,
+                `active`        TINYINT(1) NULL DEFAULT '0',
+                `name`          VARCHAR(255) NOT NULL,
+                `street`        VARCHAR(255) NOT NULL,
+                `post_code`     VARCHAR(255) NOT NULL,
+                `city`          VARCHAR(255) NOT NULL,
+                `url`           VARCHAR(255) NULL,
+                `telephone`     VARCHAR(255) NULL,
+                `open_times`    LONGTEXT NULL,
+                `country_id`    BINARY(16) NULL,
+                `created_at`    DATETIME(3),
+                `updated_at`    DATETIME(3),
+                PRIMARY KEY (`id`),
+                KEY `fk.ingos_ingorance.country_id` (`country_id`),
+                CONSTRAINT `fk.ingos_ingorance.country_id` FOREIGN KEY (`country_id`)
+                    REFERENCES `country` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
+            ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+        ");
     }
 
     public function updateDestructive(Connection $connection): void
     {
-        // implement update destructive
     }
 }
