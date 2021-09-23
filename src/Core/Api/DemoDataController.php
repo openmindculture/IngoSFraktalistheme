a95acfdeb606f9a528bc9332e0fc859ec7b2d799
Ingo Steinke <ingosteinke@gmx.de>
2021-09-23 20:48:54 +0200

    constructor for DemoDataController

--- src/Core/Api/DemoDataController.php
+++ src/Core/Api/DemoDataController.php
@@ -1,9 +1,29 @@
 <?php declare(strict_types=1);
 
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /** RouteScope
  * @RouteScope(scopes={"api"})
  */
-class DemoDataController extends AbstractController {}
+class DemoDataController extends AbstractController {
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $countryRepository;
+
+    /**
+     * @var EntityRepositoryInterface
+     */
+    private $ingoranceRepository;
+
+    public function __construct(
+        EntityRepositoryInterface $countryRepository,
+        EntityRepositoryInterface $ingoranceRepository
+    )
+    {
+        $this->countryRepository = $countryRepository;
+        $this->ingoranceRepository = $ingoranceRepository;
+    }
+}
