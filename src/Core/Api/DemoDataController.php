715825074053457bdc05350f7f5b0ca94c4d1408
Ingo Steinke <ingosteinke@gmx.de>
2021-09-24 10:59:11 +0200

    implement active country search

--- src/Core/Api/DemoDataController.php
+++ src/Core/Api/DemoDataController.php
@@ -1,15 +1,21 @@
 <?php declare(strict_types=1);
 
+use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Exception\InconsistentCriteriaIdsException;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Shopware\Core\System\Country\CountryEntity;
+use Shopware\Core\System\Country\Exception\CountryNotFoundException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
 /** RouteScope
  * @RouteScope(scopes={"api"})
  */
-class DemoDataController extends AbstractController {
+class DemoDataController extends AbstractController
+{
     /**
      * @var EntityRepositoryInterface
      */
@@ -34,8 +40,23 @@ class DemoDataController extends AbstractController {
 
     }
 
-    private function getActiveCountry(): CountryEntity
+    /**
+     * @param Context $context
+     * @return CountryEntity
+     * @throws CountryNotFoundException
+     * @throws InconsistentCriteriaIdsException
+     */
+    private function getActiveCountry(Context $context): CountryEntity
     {
+        $criteria = new Criteria();
+        $criteria->addFilter(new EqualsFilter('active', '1'));
+        $criteria->setLimit(1);
 
+        $country = $this->countryRepository->search($criteria, $context)->getEntities()->first();
+        if ($country === null) {
+            throw new CountryNotFoundException('');
+        }
+
+        return $country;
     }
 }
