e5faa39b158f80ed69a91eb40cbf9fb483919f07
Ingo Steinke <ingosteinke@gmx.de>
2021-10-04 17:02:43 +0200

    implement footer subscriber

--- src/Storefront/Subscriber/FooterSubscriber.php
+++ src/Storefront/Subscriber/FooterSubscriber.php
@@ -2,7 +2,10 @@
 
 namespace IngoSFraktalistheme\Storefront\Subscriber;
 
+use IngoSFraktalistheme\Core\Content\Ingorance\IngoranceCollection;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Recovery\Common\Service\SystemConfigService;
 use Shopware\Storefront\Pagelet\Footer\FooterPageletLoadedEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
@@ -10,12 +13,16 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 class FooterSubscriber implements EventSubscriberInterface
 {
 
+    private $systemConfigService;
+    private $ingoranceRepository;
 
     public function __construct(
         SystemConfigService $systemConfigService,
         EntityRepositoryInterface $ingoranceService
     )
     {
+        $this->systemConfigService = $systemConfigService;
+        $this->ingoranceRepository = $ingoranceService;
     }
 
     public static function getSubscribedEvents()
@@ -26,5 +33,26 @@ class FooterSubscriber implements EventSubscriberInterface
     }
 
     public function onFooterPageletLoaded(FooterPageletLoadedEvent $event): void
-    {}
+    {
+        if(!$this->systemConfigService->get('IngosFraktalistheme.config.showInStorefront')) {
+            return;
+        }
+
+        $shops = $this->fetchShops($event->getContext());
+
+        $event->getPagelet()->addExtension('ingos_fraktalistheme', $shops);
+    }
+
+    private function fetchShops(Context $context): IngoranceCollection
+    {
+        $criteria = new Criteria();
+        $criteria->addAssociation('country');
+        $criteria->addFilter(new EqualsFilter('active', 1));
+        $criteria->setLimit(5);
+
+        /** @var IngoranceCollection $ingoranceCollection */
+        $ingoranceCollection = $this->ingoranceRepository->search($criteria, $context)->getEntities();
+
+        return $ingoranceCollection;
+    }
 }
